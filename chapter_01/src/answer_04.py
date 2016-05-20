3890d434a9adfe1e8c6670555e59dc1280d797b8
Sergio Laguna <serg.o.34@gmail.com>
2016-05-20 17:04:58 +0200

    Add solution for question 1.4

--- chapter_01/src/answer_04.py
+++ chapter_01/src/answer_04.py
@@ -1,2 +1,13 @@
 def is_palindrome_permutation(palindrome_permutation):
-    pass
+    if palindrome_permutation:
+        freq = {}
+        for char in palindrome_permutation:
+            if char != ' ':
+                if char not in freq:
+                    freq[char] = 1
+                else:
+                    freq[char] += 1
+        odd_f = [f for f in freq.values() if f%2 == 1]
+        if len(odd_f) < 2:
+            return True
+    return False
