86823a533e6ed0042b1dc8f5f2344485efbfcf0e
Sergio Laguna <serg.o.34@gmail.com>
2016-05-20 17:00:50 +0200

    Add solution for question 1.2

--- chapter_01/src/answer_02.py
+++ chapter_01/src/answer_02.py
@@ -1,2 +1,8 @@
 def is_permutation(fst_str, snd_str):
-    pass
+    if not fst_str or not snd_str:
+        return False
+    a = list(fst_str)
+    b = list(snd_str)
+    a.sort()
+    b.sort()
+    return a == b
