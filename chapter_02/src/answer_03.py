b29245723080f28adb9b1769d7eccb33ba0198ef
Maite Giménez <mai@immutable.es>
2016-10-31 18:19:21 +0100

    There was an error with this file. I have created it as a python testfile instead of a python source file.

--- chapter_02/src/answer_03.py
+++ chapter_02/src/answer_03.py
@@ -1,10 +1,2 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def delete_node(node_to_delete):
+    pass 
