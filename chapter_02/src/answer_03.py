ca9edc8b10b170fc7d775e8c24c5a28da0f1f501
Maite Giménez <mai@immutable.es>
2016-10-31 19:17:52 +0100

    I failed miserably in the rebase. This was an error.

--- chapter_02/src/answer_03.py
+++ chapter_02/src/answer_03.py
@@ -1,11 +1,7 @@
 def delete_node(node_to_delete):
-<<<<<<< 33a8a45b0d4a407a901bd2d99a710ce6fdb4ecfd
-    pass 
-=======
     if node_to_delete is None or node_to_delete._next is None:
         return False
     else:
         node_to_delete._data = node_to_delete._next._data
         node_to_delete._next = node_to_delete._next._next
         return True
->>>>>>> Solutions to the second chapter.
