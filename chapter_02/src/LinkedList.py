33a8a45b0d4a407a901bd2d99a710ce6fdb4ecfd
Maite Giménez <mai@immutable.es>
2016-10-31 17:41:37 +0100

    Added a pointer to the tail of the LinkedList in order to be able to test circular lists

--- chapter_02/src/LinkedList.py
+++ chapter_02/src/LinkedList.py
@@ -12,20 +12,40 @@ class LinkedList(object):
     def __init__(self, list_ints=None):
         # TODO Make this protected (@property)
         self._head = None
+        self._tail = None
         if list_ints:
             for int_data in list_ints:
                 self.append_to_tail(int_data)
 
     def append_to_tail(self, int_data):
-        """ Add a new node at the end of the Linked List """
+        """ Add a new node with the data at the end of the Linked List """
         last_node = Node(int_data)
         if not self._head:
             self._head = last_node
+            self._tail = last_node
         else:
             current_node = self._head
             while current_node._next is not None:
                 current_node = current_node._next
             current_node._next = last_node
+            self._tail = last_node
+        return last_node
+
+    def append_node(self, new_node):
+        """ Add a the new node passed at the end of the Linked List """
+        if not self._head:
+            self._head = new_node
+        else:
+            current_node = self._head
+            while current_node._next is not None:
+                current_node = current_node._next
+            current_node._next = new_node
+
+    def append_list(self, tail_list):
+        current_node = self._head
+        while current_node._next is not None:
+            current_node = current_node._next
+        current_node._next = tail_list._head
 
     def delete_node(self, int_data):
         """ It deletes every node with the int_data as value"""
